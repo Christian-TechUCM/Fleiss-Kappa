48eb14275c1b507d9cea66aa8a61dcaa3b9be556
Christian Urbina <urbinachris@gmail.com>
2022-09-25 18:58:24 -0700

    Sync

--- IRR.py
+++ IRR.py
@@ -1,6 +1,26 @@
-import os
 import pandas as pd
 
-data = pd.read_excel (r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles\2.xlsx')
-df = pd.DataFrame(data, columns=['L','Ind','CG','WG','OG','AnQ','SQ','WC','Prd','SP','T/Q','W','O','Lec','RtW','FUp','PQ','CQ','AnQ','MG','1o1','D/V','Adm','W','O'])
-print (df)
+
+def ReadData():
+   
+   #Read multiple excel files and and store them in a matrix
+    #Read the first excel file from testfiles folder
+    df1 = pd.read_excel(r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles\1.xlsx', sheet_name='Sheet1')
+    #Read the second excel file from testfiles folder
+    df2 = pd.read_excel(r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles\2.xlsx', sheet_name='Sheet1')
+
+    #Create a list of dataframes
+    df_list = [df1, df2]
+
+    print(df_list)
+
+
+
+
+
+def main():
+    ReadData()
+
+
+if __name__ == "__main__":
+    main()
