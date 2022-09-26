27298b4f238e3eb40d5f3cc48e428e094292a1b3
Christian Urbina <urbinachris@gmail.com>
2022-09-25 19:53:06 -0700

    Week 1

--- IRR.py
+++ IRR.py
@@ -1,24 +1,38 @@
+from codecs import charmap_build
 import pandas as pd
+import os
+import krippendorff
+fields = ['L','Ind','CG','WG','OG','AnQ','SQ','WC','Prd','SP','T/Q','W','O','Lec','RtW','FUp','PQ','CQ','AnQ','MG','1o1','D/V','Adm','W','O']
+
+
+
+def SetWorkingDirectory():
+    # Ask the user to set the working directory and save as char
+    working_directory = input("Please enter the working directory: ")
+    # Change the working directory to the one entered by the user
+    os.chdir(working_directory)
+
+    # Set the working directory
+   # os.chdir(r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles')
 
 
 def ReadData():
-   
-   #Read multiple excel files and and store them in a matrix
-    #Read the first excel file from testfiles folder
-    df1 = pd.read_excel(r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles\1.xlsx', sheet_name='Sheet1')
-    #Read the second excel file from testfiles folder
-    df2 = pd.read_excel(r'C:\Users\urbin\Documents\Code\COPUS-IRR\Testfiles\2.xlsx', sheet_name='Sheet1')
+    # Loop through the files in the working directory and read excel sheets into a dataframe using specified columns
+    for file in os.listdir():
+        if file.endswith(".xlsx"):
+            df = pd.read_excel(file, usecols=fields,  header=0)
+            print(df)
 
-    #Create a list of dataframes
-    df_list = [df1, df2]
-
-    print(df_list)
+def krippendorff_alpha(data, level_of_measurement):
+    return krippendorff.alpha(data, level_of_measurement=level_of_measurement)
+    
 
 
-
+# , usecols=fields ['L','Ind','CG','WG','OG','AnQ','SQ','WC','Prd','SP','T/Q','W','O','Lec','RtW','FUp','PQ','CQ','AnQ','MG','1o1','D/V','Adm','W','O']
 
 
 def main():
+    SetWorkingDirectory()
     ReadData()
 
 
